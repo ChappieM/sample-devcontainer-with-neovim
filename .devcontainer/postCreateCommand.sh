43f32d1fb55be9ecc1cec4315bf37fd3f0a62f0d
chappiem <cha39881@gmail.com>
2023-06-08 00:46:44 +0900

    feat: add my nvim settings
    '

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-
+# install neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
 rm nvim.appimage
 sudo mv squashfs-root /
 sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+
+# clone my nvim config
+nvim_dir=$HOME/.config/nvim
+if [ -d $nvim_dir ]; then
+	mv $nvim_dir ~/.config/fish_old
+fi
+git clone https://github.com/ChappieM/nvim.git $nvim_dir
